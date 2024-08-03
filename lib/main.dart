89e2e40c32957052749616ea45ea3fd3dd03ff80
MuhammadTalha17 <121374986+MuhammadTalha17@users.noreply.github.com>
2024-08-03 12:17:17 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Greet extends StatefulWidget {
+  const Greet({super.key});
+
+  @override
+  State<Greet> createState() => _GreetState();
+}
+
+class _GreetState extends State<Greet> {
+  //State Variable:
+  var name = "";
+
+  @override
+  Widget build(BuildContext context) {
+    var greetStyle = const TextStyle(fontSize: 25);
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Text(
+                "Assalamu Alaikum $name",
+                style: greetStyle,
+              ),
+              Text(
+                "!!!",
+                style: greetStyle,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+          child: TextField(
+              onChanged: (value) => setState(() {
+                    name = value;
+                  })),
+        ),
+      ],
+    );
+  }
+}
+
+class SalamWorld extends StatelessWidget {
+  const SalamWorld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Salam World!");
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coffee Masters',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 3, 20, 56)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Image.asset("images/logo.png"),
+      ),
+      body: Greet(),
+    );
+  }
+}
