ce39a032e5af49f43cb6d9709365891cd66be5ff
MuhammadTalha17 <121374986+MuhammadTalha17@users.noreply.github.com>
2024-08-05 11:15:43 +0500

    ProductItem, layout and Button

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_masters/datamodel.dart';
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget {
@@ -5,6 +6,79 @@ class MenuPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var p = Product(id: 1, name: "Dummy Product", price: 1.75, image: "image");
+    var q = Product(
+        id: 1, name: "Another Dummy Product", price: 2.25, image: "image");
+    return ListView(
+      children: [
+        ProductItem(
+          product: p,
+          onAdd: () {},
+        ),
+        ProductItem(
+          product: q,
+          onAdd: () {},
+        ),
+      ],
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  final Function onAdd;
+
+  const ProductItem({super.key, required this.product, required this.onAdd});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Card(
+        elevation: 5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("images/black_coffee.png"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        product.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("\$${product.price}"),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 25.0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(Colors.brown),
+                    ),
+                    onPressed: () {
+                      onAdd(product);
+                    },
+                    child: const Text(
+                      "Add",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
